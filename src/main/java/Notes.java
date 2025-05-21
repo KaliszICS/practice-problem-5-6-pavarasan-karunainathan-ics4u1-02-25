70372f99888da07114d1e5f9f8c5cb6a046f0487
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-05-21 14:26:12 +0000

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -2,5 +2,14 @@
 class Notes {
 	public static void main(String[] args) {
 
+		// Inheritance - is-A releationship
+
+		//Composition - has-A releationship
+
+		//Favor Composition over Inheritance.
+
+		//Is my code efficient
+		//Is my code easy to read
+		//Is my code easy to edit
 	}
 }
